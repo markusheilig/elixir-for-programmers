d9bd73c891ad88a35e8a7fd42d0c4302a924a034
Markus Heilig <mheilig@hcmb0152010322.hc.lan>
2019-04-15 09:13:40 +0200

    add dictionary-app for hangman-game

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,22 @@
+defmodule Dictionary do
+  # execute a function
+
+  # 1. using mix:
+  # mix run -e Dictionary.hello
+
+  # 2. using iex:
+  # iex -S mix, then execute Dictionary.hello in iex
+  # recompile module: r Dictionary
+  # compile file: c "lib/dictionary.ex"
+
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+
+  def word_list() do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+end
